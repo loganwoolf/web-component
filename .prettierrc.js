82d45505e823861e2419d4b81a454c1190cb1390
Logan Woolf <loganwoolf@gmail.com>
2023-12-22 23:25:58 -0700

    Initial commit
    Set up vite and prettier
    Add webcomponent code to project

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+    arrowParens: 'always',
+    trailingComma: 'all',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+};
+
+export default config;
